d4d8fcbe0e445fd87529801e592a6810a050eff0
Steve Conover <sconover@gmail.com>
2010-09-26 08:41:15 -0700

    a friendly message

--- test/suite.js
+++ test/suite.js
@@ -8,6 +8,6 @@ for (var i = 0, len = files.length; i < len; ++i){
 var exec = require('child_process').exec;
 exec("sh -c 'cd lib && modulrize arel.js > ../arel-all.js'", function (error, stdout, stderr) {
   if (error !== null) {
-    console.log('error creating arel-all.js: ' + error);
+    console.log('error creating arel-all.js, make sure you have modulr installed (gem install modulr): ' + error);
   }
 });

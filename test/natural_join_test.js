403f166608c6732bc60b93ea00613ece6bdfa32e
Steve Conover <sconover@gmail.com>
2010-09-29 22:04:58 -0700

    get rid of the heading layer.  a little more human-friendly now.

---
+++ test/natural_join_test.js
@@ -0,0 +1,31 @@
+require("./test_helper.js");
+require("arel/natural_join")
+
+regarding("arel.NaturalJoin", function () {
+    
+  beforeEach(function() {
+    person = new arel.Relation("person")
+              .attr("id", arel.Attribute.IntegerType)
+              .attr("house_id", arel.Attribute.IntegerType)
+              .attr("name", arel.Attribute.StringType)      
+              .attr("age", arel.Attribute.IntegerType);      
+
+    house = new arel.Relation("house")
+              .attr("house_id", arel.Attribute.IntegerType)
+              .attr("address", arel.Attribute.StringType);
+
+    join = new arel.NaturalJoin(person, house);
+  });
+
+  
+  test("concatenates the relation names together to make a default name", function (){
+    assert.equal("person__house", join.name());
+  });
+    
+  test("combines the attributes of the two relations [THIS IS WRONG, REALLY]", function (){
+    names = _.map(join.attributes(), function(attr){return attr.name()});
+    assert.equal(["id", "house_id", "name", "age", "house_id", "address"], names);
+  });
+
+});
+
--- test/relation_test.js
+++ test/relation_test.js
@@ -2,22 +2,6 @@ require("./test_helper.js");
 require("arel/relation")
 
 regarding("arel.Relation", function () {
-
-  regarding("a relation has a heading", function () {
-    
-    test("a heading has attributes.  the heading is enumerable.", function (){
-      
-      var name = new arel.Attribute("name", arel.Attribute.StringType);      
-      var age = new arel.Attribute("age", arel.Attribute.IntegerType);
-      
-      names = _.map(new arel.Relation.Heading([name, age]), function(attr){return attr.name()})
-      types = _.map(new arel.Relation.Heading([name, age]), function(attr){return attr.type()})
-    
-      assert.equal(names, ["name", "age"]);
-      assert.equal([arel.Attribute.StringType, arel.Attribute.IntegerType], types);
-    });
-
-  });
   
   regarding("tuples", function () {
     test("tuples are available only in specialized relations", function (){

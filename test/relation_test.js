d93348e17d9111a77e6c7342841cb6884ae2ef85
Steve Conover <sconover@gmail.com>
2010-09-29 18:52:56 -0700

    tuples aren't available in the abstract relation class

--- test/relation_test.js
+++ test/relation_test.js
@@ -5,7 +5,7 @@ regarding("arel.Relation", function () {
 
   regarding("a relation has a heading", function () {
     
-    test('a heading has attributes.  the heading is enumerable.', function () {
+    test("a heading has attributes.  the heading is enumerable.", function (){
       
       var name = new arel.Attribute("name", arel.Attribute.StringType);      
       var age = new arel.Attribute("age", arel.Attribute.IntegerType);
@@ -18,6 +18,13 @@ regarding("arel.Relation", function () {
     });
 
   });
-
+  
+  regarding("tuples", function () {
+    test("tuples are available only in specialized relations", function (){
+      assert.throws(function(){new arel.Relation("foo").tuples()}, 
+                    Error, 
+                    "tuples are available only in specialized relations");
+    });
+  });
 });
 

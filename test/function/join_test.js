23ecdeafa62bf5ef94812ad0929759653d265442
Steve Conover <sconover@gmail.com>
2010-10-31 00:10:19 -0700

    more conversion to new assert methods

--- test/function/join_test.js
+++ test/function/join_test.js
@@ -42,58 +42,53 @@ regarding("join", function() {
   regarding("sameness and equivalence", function() {
     
     test("same", function (){knit(function(){
-      assert.equal(true, join(person, house).isSame(join(person, house)))
+      assert.same(join(person, house), join(person, house))
 
-      assert.equal(false, join(person, house).isSame(join(house, city)))
-      assert.equal(false, join(house, city).isSame(join(person, house)))
+      assert.notSame(join(person, house), join(house, city))
+      assert.notSame(join(house, city), join(person, house))
     })})    
 
     test("same - with predicate", function (){knit(function(){
-      assert.equal(true, join(person, house, equality(person.attr("house_id"), house.attr("house_id"))).
-                           isSame(join(person, house, equality(person.attr("house_id"), house.attr("house_id")))))
+      assert.same(join(person, house, equality(person.attr("house_id"), house.attr("house_id"))),
+                  join(person, house, equality(person.attr("house_id"), house.attr("house_id"))))
 
-      assert.equal(false, join(person, house, equality(person.attr("name"), house.attr("house_id"))).
-                           isSame(join(person, house, equality(person.attr("house_id"), house.attr("house_id")))))
+      assert.notSame(join(person, house, equality(person.attr("name"), house.attr("house_id"))),
+                     join(person, house, equality(person.attr("house_id"), house.attr("house_id"))))
     })})
 
     test("same implies equivalent", function (){knit(function(){
-      assert.equal(true, join(person, house).isEquivalent(join(person, house)))
+      assert.equivalent(join(person, house), join(person, house))
     })})
 
     test("equivalent with predicate", function (){knit(function(){
-      assert.equal(true, join(person, house, equality(person.attr("house_id"), house.attr("house_id"))).
-                           isEquivalent(join(person, house, equality(person.attr("house_id"), house.attr("house_id")))))
+      assert.equivalent(join(person, house, equality(person.attr("house_id"), house.attr("house_id"))),
+                        join(person, house, equality(person.attr("house_id"), house.attr("house_id"))))
 
-      assert.equal(true, join(person, house, equality(house.attr("house_id"), person.attr("house_id"))).
-                           isEquivalent(join(person, house, equality(person.attr("house_id"), house.attr("house_id")))))
+      assert.equivalent(join(person, house, equality(house.attr("house_id"), person.attr("house_id"))),
+                        join(person, house, equality(person.attr("house_id"), house.attr("house_id"))))
     })})
 
     test("commutativity - two join functions are equivalent if the relations are the same but in different order", function (){knit(function(){
-      assert.equal(true, join(person, house).isEquivalent(join(house, person)))
-      
-      assert.equal(false, join(person, house).isEquivalent(join(person, city)))
+      assert.equivalent(join(person, house), join(house, person))
+      assert.notEquivalent(join(person, house), join(person, city))
     })})
 
     test("nonassociativity - order of operations (inner/outer) matters", function (){knit(function(){
-      assert.equal(true, join(join(person, house), city).isEquivalent(join(join(person, house), city)))
-      
-      assert.equal(false, join(join(person, house), city).isEquivalent(join(person, join(house, city))))
+      assert.equivalent(join(join(person, house), city), join(join(person, house), city))
+      assert.notEquivalent(join(join(person, house), city), join(person, join(house, city)))
     })})
     
   })
   
   regarding("appendToPredicate", function() {
     test("when there's only a True predicate existing, replace it", function (){knit(function(){
-      assert.equal(true, join(person, house).appendToPredicate(equality(4,5)).
-                           isSame(join(person, house, equality(4,5))))
-      
-      assert.equal(true, join(person, house, TRUE).appendToPredicate(equality(4,5)).
-                           isSame(join(person, house, equality(4,5))))
+      assert.same(join(person, house).appendToPredicate(equality(4,5)), join(person, house, equality(4,5)))
+      assert.same(join(person, house, TRUE).appendToPredicate(equality(4,5)), join(person, house, equality(4,5)))
     })})	  
 
     test("when there's any other kind of existing predicate, make a conjunction", function (){knit(function(){
-      assert.equal(true, join(person, house, equality(4,5)).appendToPredicate(equality(6,7)).
-                           isSame(join(person, house, conjunction(equality(4,5), equality(6,7)))))
+      assert.same(join(person, house, equality(4,5)).appendToPredicate(equality(6,7)),
+                  join(person, house, conjunction(equality(4,5), equality(6,7))))
     })})	  
 	})
 })

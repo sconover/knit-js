7863df1bf377791b8c0eff512d4059fb290e71ad
Steve Conover <sconover@gmail.com>
2010-11-01 23:42:28 -0700

    where - and, eq

--- test/engine/sql/select_statement_test.js
+++ test/engine/sql/select_statement_test.js
@@ -17,12 +17,21 @@ regarding("select statement", function() {
 			assert.equal("select * from person", select.toSql())
 		})
 
-		xtest("where", function(){knit(function(){
-			select.
-			  from("person").
-			  where()
-			assert.equal("select * from person", select.toSql())
-		})})
+		test("where - and,eq", function(){
+
+			select.modify(function(){
+				from("person")				
+				where(eq("name", "'Jane'"))
+			})
+
+			assert.equal("select * from person where name = 'Jane'", select.toSql())
+			
+			select.modify(function(){
+				where(eq("hair", "'Red'"))
+			})
+			
+			assert.equal("select * from person where name = 'Jane' and hair = 'Red'", select.toSql())
+		})
 
 	})
 	

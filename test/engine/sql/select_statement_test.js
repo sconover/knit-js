81d159188576ba9bb7383ed0e7e14e0a47b0a4b5
Steve Conover <sconover@gmail.com>
2010-11-02 22:16:36 -0700

    select statement - join

--- test/engine/sql/select_statement_test.js
+++ test/engine/sql/select_statement_test.js
@@ -17,21 +17,52 @@ regarding("select statement", function() {
 			assert.equal("select * from person", select.toSql())
 		})
 
-		test("where - and,eq", function(){
+    test("where - and,eq", function(){
 
-			select.modify(function(){
-				from("person")				
-				where(eq("name", "'Jane'"))
-			})
+      select.modify(function(){
+        from("person")        
+        where(eq("name", "'Jane'"))
+      })
 
-			assert.equal("select * from person where name = 'Jane'", select.toSql())
-			
-			select.modify(function(){
-				where(eq("hair", "'Red'"))
-			})
-			
-			assert.equal("select * from person where name = 'Jane' and hair = 'Red'", select.toSql())
-		})
+      assert.equal("select * from person where name = 'Jane'", select.toSql())
+      
+      select.modify(function(){
+        where(eq("hair", "'Red'"))
+      })
+      
+      assert.equal("select * from person where name = 'Jane' and hair = 'Red'", select.toSql())
+    })
+
+    test("simple join", function(){
+
+      select.modify(function(){
+        from("person")
+        join("house")
+      })
+
+      assert.equal("select * from person join house", select.toSql())
+    })
+
+    test("join with predicate", function(){
+
+      select.modify(function(){
+        from("person")
+        join("house", eq("person.house_id", "house.house_id"))
+      })
+
+      assert.equal("select * from person join house on person.house_id = house.house_id", select.toSql())
+    })
+
+    test("multiple joins", function(){
+
+      select.modify(function(){
+        from("person")
+        join("house", eq("person.house_id", "house.house_id"))
+        join("city")
+      })
+
+      assert.equal("select * from person join house on person.house_id = house.house_id join city", select.toSql())
+    })
 
 	})
 	

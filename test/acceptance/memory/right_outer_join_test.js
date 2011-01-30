552156f796e2f65108b6690d07efd35bf787e806
Steve Conover <sconover@gmail.com>
2011-01-30 12:19:08 -0800

    right outer join

---
+++ test/acceptance/memory/right_outer_join_test.js
@@ -0,0 +1,42 @@
+require("../helper")
+require("knit/engine/memory")
+
+regarding("In Memory Engine", function() {
+    
+  beforeEach(function() {
+    _.bind(setupAcceptanceFixtures, this)(new knit.engine.Memory())
+    
+    this.house.merge([
+      [104, "Ashbury", 1001]
+    ])
+
+  })
+
+  regarding("right outer join", function() {
+
+    test("create joined rows where the predicate matches, " +
+         "or if there's no predicate match for to a row on the left, " +
+         "return a joined row that consists of the row on the right with nulls on the left", function (){
+      
+      allPeopleCombinedWithAllHouses = this.$R(function(){
+        return rightOuterJoin(relation("person"), relation("house"), equality(attr("person.houseId"), attr("house.houseId")))
+      }).perform()
+      
+      assert.equal({
+        name:"person__house",
+        attributes:["personId", "houseId", "name", "age", "houseId", "address", "cityId"],
+        rows:[
+          [1, 101, "Jane", 5, 101, "Chimney Hill", 1001],
+          [2, 101, "Puck", 12, 101, "Chimney Hill", 1001],
+          [3, 102, "Fanny", 30, 102, "Parnassus", 1001],
+          [4, 103, "Amy", 6, 103, "Canal", 1002],
+          [null, null, null, null, 104, "Ashbury", 1001],
+        ]
+      }, relationContents(allPeopleCombinedWithAllHouses))
+        
+    })
+  })
+  
+  
+})
+

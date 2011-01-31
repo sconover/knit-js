1a3130b68368bca819071932b9976b0923766b92
Steve Conover <sconover@gmail.com>
2011-01-30 20:23:04 -0800

    natural join

---
+++ test/acceptance/memory/natural_join_test.js
@@ -0,0 +1,35 @@
+require("../helper")
+require("knit/engine/memory")
+
+regarding("In Memory Engine", function() {
+    
+  beforeEach(function() {
+    _.bind(setupAcceptanceFixtures, this)(new knit.engine.Memory())
+  })
+
+  regarding("natural join", function() {
+
+    test("natural join automatically creates a join predicate based on like-named columns from both tables. " +
+         "in a departure from 'purity', only columns ending in Id are considered. We'll see how this goes.", function (){
+      
+      allPeopleCombinedWithAllHouses = this.$R(function(){
+        return naturalJoin(relation("person"), relation("house"))
+      }).perform()
+      
+      assert.equal({
+        name:"person__house",
+        attributes:["personId", "houseId", "name", "age", "houseId", "address", "cityId"],
+        rows:[
+          [1, 101, "Jane", 5, 101, "Chimney Hill", 1001],
+          [2, 101, "Puck", 12, 101, "Chimney Hill", 1001],
+          [3, 102, "Fanny", 30, 102, "Parnassus", 1001],
+          [4, 103, "Amy", 6, 103, "Canal", 1002]
+        ]
+      }, relationContents(allPeopleCombinedWithAllHouses))
+        
+    })
+  })
+  
+  
+})
+

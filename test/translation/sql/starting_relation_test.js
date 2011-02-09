dc1024726a223224cb14bb9d3065621568dfc3c4
Steve Conover <sconover@gmail.com>
2011-02-08 16:53:26 -0800

    rf.  100 tests in 100ms

--- test/translation/sql/starting_relation_test.js
+++ test/translation/sql/starting_relation_test.js
@@ -7,30 +7,34 @@ regarding("starting relation", function() {
   beforeEach(function(){ setupPersonHouseCity(this, function(name, attributeNames){return new FakeTable(name, attributeNames)}) })
   var sql = knit.translation.sql
   
-  test("convert a straight relation reference to sql", function(){
-    var relation = this.$R(function(){
-      return relation("person")
+  regarding("translate relation to sql object", function(){
+    
+    test("convert a straight relation reference to sql", function(){
+      var relation = this.$R(function(){
+        return relation("person")
+      })
+      assert.equal(
+        new sql.Select().
+          from("person"),
+        relation.toSql()
+      )
     })
-    assert.equal(
-      new sql.Select().
-        from("person"),
-      relation.toSql()
-    )
+    
   })
   
-  regarding("to statement", function(){
+  regarding("sql object to statement", function(){
 
     test("simple select statement.  what defaults to star.", function(){
       assert.equal(
         "select * from person",
-        new sql.Select().from("person").toStatement()
+        new sql.Select().from("person").toStatement().sql
       )
     })
     
     test("multiple froms", function(){
       assert.equal(
         "select * from person, house",
-        new sql.Select().from("person", "house").toStatement()
+        new sql.Select().from("person", "house").toStatement().sql
       )
     })
     

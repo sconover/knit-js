dde22f8115e2b88f8297bab1dfe28481204b4155
Steve Conover <sconover@gmail.com>
2011-02-08 22:22:25 -0800

    rudimentary join to sql

--- test/translation/sql/join_test.js
+++ test/translation/sql/join_test.js
@@ -7,25 +7,50 @@ regarding("join to sql", function() {
   beforeEach(function(){ setupPersonHouseCity(this, function(name, attributeNames){return new FakeTable(name, attributeNames)}) })
   var sql = knit.translation.sql
   
-  test("cartesian", function(){
-    var join = this.$R(function(){
-      return join(relation("person"), relation("house"))
+  regarding("expression to sql object", function() {
+    
+    test("cartesian", function(){
+      var join = this.$R(function(){
+        return join(relation("person"), relation("house"))
+      })
+      assert.equal(
+        new sql.Select().
+          join(new sql.Join("person", "house", null)),
+        join.toSql()
+      )
     })
-    assert.equal(
-      new sql.Select().
-        join(new sql.Join("person", "house", new sql.predicate.Equals(1,1))),
-      join.toSql()
-    )
-  })
 
-  test("with predicate", function(){
-    var join = this.$R(function(){
-      return join(relation("person"), relation("house"), eq(attr("house.houseId"), attr("person.houseId")))
+    test("with predicate", function(){
+      var join = this.$R(function(){
+        return join(relation("person"), relation("house"), eq(attr("house.houseId"), attr("person.houseId")))
+      })
+      assert.equal(
+        new sql.Select().
+          join(new sql.Join("person", "house", new sql.predicate.Equals(new sql.Column("house.houseId"), new sql.Column("person.houseId")))),
+        join.toSql()
+      )
     })
-    assert.equal(
-      new sql.Select().
-        join(new sql.Join("person", "house", new sql.predicate.Equals(new sql.Column("house.houseId"), new sql.Column("person.houseId")))),
-      join.toSql()
-    )
-  })  
+      
+  })
+  
+  regarding("sql object to sql statement", function() {
+  
+    test("cartesian", function(){
+      assert.equal(
+        "select * from person join house",
+        new sql.Select().
+          join(new sql.Join("person", "house", null)).toStatement().sql
+      )
+    })
+
+    test("with predicate", function(){
+      assert.equal(
+        "select * from person join house on house.houseId = person.houseId",
+        new sql.Select().
+          join(new sql.Join("person", "house", new sql.predicate.Equals(new sql.Column("house.houseId"), new sql.Column("person.houseId")))).
+          toStatement().sql
+      )
+    })    
+    
+  })
 })

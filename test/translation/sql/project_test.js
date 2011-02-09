0cab614e834485bc822e538ef9c3c29285b29eb3
Steve Conover <sconover@gmail.com>
2011-02-08 17:05:47 -0800

    project/what, to statement

--- test/translation/sql/project_test.js
+++ test/translation/sql/project_test.js
@@ -6,17 +6,34 @@ regarding("project", function() {
   
   beforeEach(function(){ setupPersonHouseCity(this, function(name, attributeNames){return new FakeTable(name, attributeNames)}) })
   var sql = knit.translation.sql
-  
-  test("convert a project(proh-JEKT) to sql", function(){
-    var project = this.$R(function(){
-      return project(relation("person"), attr("person.name", "person.age"))
+
+  regarding("translate relation to sql object", function(){  
+
+    test("convert a project(proh-JEKT) to sql", function(){
+      var project = this.$R(function(){
+        return project(relation("person"), attr("person.name", "person.age"))
+      })
+      assert.equal(
+        new sql.Select().
+          what(new sql.Column("person.name"), new sql.Column("person.age")).
+          from("person"),
+        project.toSql()
+      )
     })
-    assert.equal(
-      new sql.Select().
-        what(new sql.Column("person.name"), new sql.Column("person.age")).
-        from("person"),
-      project.toSql()
-    )
+
+  })
+  
+  regarding("sql object to statement", function(){
+
+    test("select clause", function(){
+      assert.equal(
+        "select person.name, person.age from person",
+        new sql.Select().
+          what(new sql.Column("person.name"), new sql.Column("person.age")).
+          from("person").toStatement().sql
+      )
+    })
+    
   })
 
 })

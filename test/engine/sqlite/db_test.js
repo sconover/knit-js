a4fb352a2436d3b8fa07adf8e71e2ce32686ba20
Steve Conover <sconover@gmail.com>
2011-02-09 11:07:17 -0800

    making the core sqlite db interface more basic

--- test/engine/sqlite/db_test.js
+++ test/engine/sqlite/db_test.js
@@ -16,36 +16,30 @@ regarding("sqlite db", function() {
     db.close()
   })
   
-  xtest("execute, query", function(){
-    db.executeSync(new knit.engine.sql.statement.CreateTable("foo", [["color", knit.engine.sql.StringType]]))    
-    db.executeSync(new knit.engine.sql.statement.Insert("foo", ["color"], ["red"]))
+  test("execute, query", function(){
+    db.execute({sql:"create table foo(color string)"})
+    db.execute({sql:"insert into foo values('red')"})
     
-    var result = db.querySync(new knit.engine.sql.statement.Select(function(){
-      from("foo")
-    }))
-    
-    var justRows = _A.map(result, function(row){return row})
-    assert.equal([{"color":"red"}], justRows)
+    assert.equal([{"color":"red"}], db.query({sql:"select * from foo"}))
   })
   
   test("list tables", function(){
-    db.executeSync(new knit.engine.sql.statement.CreateTable("foo", [["color", knit.engine.sql.StringType]]))    
-    db.executeSync(new knit.engine.sql.statement.CreateTable("bar", [["age", knit.engine.sql.IntegerType]]))    
-    
+    db.execute({sql:"create table foo(color string)"})
+    db.execute({sql:"create table bar(age int)"})    
     assert.equal(["bar", "foo"], db.listTables())
   })
   
-  test("table definition", function(){
-    db.executeSync(new knit.engine.sql.statement.CreateTable("foo", [["color", knit.engine.sql.StringType]]))    
-    db.executeSync(new knit.engine.sql.statement.CreateTable("bar", [["age", knit.engine.sql.IntegerType]]))    
-    
-    assert.equal(new knit.engine.sql.statement.CreateTable("foo", [["color", knit.engine.sql.StringType]]), 
-                 db.tableDefinition("foo"))
-                 
-    assert.equal(new knit.engine.sql.statement.CreateTable("bar", [["age", knit.engine.sql.IntegerType]]), 
-                 db.tableDefinition("bar"))
-
-  })
+  // xtest("table definition", function(){
+  //   db.executeSync(new knit.engine.sql.statement.CreateTable("foo", [["color", knit.engine.sql.StringType]]))    
+  //   db.executeSync(new knit.engine.sql.statement.CreateTable("bar", [["age", knit.engine.sql.IntegerType]]))    
+  //   
+  //   assert.equal(new knit.engine.sql.statement.CreateTable("foo", [["color", knit.engine.sql.StringType]]), 
+  //                db.tableDefinition("foo"))
+  //                
+  //   assert.equal(new knit.engine.sql.statement.CreateTable("bar", [["age", knit.engine.sql.IntegerType]]), 
+  //                db.tableDefinition("bar"))
+  // 
+  // })
   
 
   

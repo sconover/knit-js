835cde18b37039aa2e1b6cfd19ba5fae6ff0ce03
Steve Conover <sconover@gmail.com>
2011-02-09 15:05:59 -0800

    column quackslike attribute

--- test/engine/sqlite/column_test.js
+++ test/engine/sqlite/column_test.js
@@ -1,33 +1,40 @@
 require("../../test_helper")
 require("knit/core")
+require("knit/engine/sqlite/table")
 require("knit/engine/sqlite/column")
 
 regarding("column", function() {
 
   var _A = CollectionFunctions.Array.functions,
       type = knit.attributeType,
-      sqlite = knit.engine.sqlite
+      sqlite = knit.engine.sqlite,
+      fooTable = new sqlite.Table("foo", []),
+      barTable = new sqlite.Table("bar", [])
+  
+  test("quackslike attribute", function(){
+    assert.quacksLike(new sqlite.Column({name:"color", type:"string"}, fooTable), knit.signature.attribute)
+  })
   
   test("sameness and equivalence", function(){
-    assert.same(new sqlite.Column({name:"color", type:"string"}, "fooTable"), 
-                new sqlite.Column({name:"color", type:"string"}, "fooTable"))
+    assert.same(new sqlite.Column({name:"color", type:"string"}, fooTable), 
+                new sqlite.Column({name:"color", type:"string"}, fooTable))
 
-    assert.notSame(new sqlite.Column({name:"color", type:"string"}, "fooTable"), 
-                   new sqlite.Column({name:"color", type:"int"}, "fooTable"))
-    assert.notSame(new sqlite.Column({name:"color", type:"string"}, "fooTable"), 
-                   new sqlite.Column({name:"name", type:"string"}, "fooTable"))
-    assert.notSame(new sqlite.Column({name:"color", type:"string"}, "fooTable"), 
-                   new sqlite.Column({name:"color", type:"string"}, "barTable"))
+    assert.notSame(new sqlite.Column({name:"color", type:"string"}, fooTable), 
+                   new sqlite.Column({name:"color", type:"int"}, fooTable))
+    assert.notSame(new sqlite.Column({name:"color", type:"string"}, fooTable), 
+                   new sqlite.Column({name:"name", type:"string"}, fooTable))
+    assert.notSame(new sqlite.Column({name:"color", type:"string"}, fooTable), 
+                   new sqlite.Column({name:"color", type:"string"}, barTable))
 
-    assert.equivalent(new sqlite.Column({name:"color", type:"string"}, "fooTable"), 
-                      new sqlite.Column({name:"color", type:"string"}, "fooTable"))
+    assert.equivalent(new sqlite.Column({name:"color", type:"string"}, fooTable), 
+                      new sqlite.Column({name:"color", type:"string"}, fooTable))
 
-    assert.notEquivalent(new sqlite.Column({name:"color", type:"string"}, "fooTable"), 
-                         new sqlite.Column({name:"color", type:"int"}, "fooTable"))
-    assert.notEquivalent(new sqlite.Column({name:"color", type:"string"}, "fooTable"), 
-                         new sqlite.Column({name:"name", type:"string"}, "fooTable"))
-    assert.notEquivalent(new sqlite.Column({name:"color", type:"string"}, "fooTable"), 
-                         new sqlite.Column({name:"color", type:"string"}, "barTable"))
+    assert.notEquivalent(new sqlite.Column({name:"color", type:"string"}, fooTable), 
+                         new sqlite.Column({name:"color", type:"int"}, fooTable))
+    assert.notEquivalent(new sqlite.Column({name:"color", type:"string"}, fooTable), 
+                         new sqlite.Column({name:"name", type:"string"}, fooTable))
+    assert.notEquivalent(new sqlite.Column({name:"color", type:"string"}, fooTable), 
+                         new sqlite.Column({name:"color", type:"string"}, barTable))
   })    
   
 })

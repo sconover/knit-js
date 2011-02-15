7fd138a40eef7cc1afcb69b6e3cdb19b321b6815
Steve Conover <sconover@gmail.com>
2011-02-15 11:17:31 -0800

    conjunction AT

--- test/acceptance/select_test.js
+++ test/acceptance/select_test.js
@@ -7,7 +7,7 @@ acceptanceTest("select", engine.memory, engine.sqlite, function(){
     test("basic equality", function (){
       var smallerRelation = 
         this.$R(function(){
-          return select(relation("person"), equality(attr("person.name"), "Fanny"))
+          return select(relation("person"), eq(attr("person.name"), "Fanny"))
         }).perform()
         
       assert.equal({
@@ -15,6 +15,21 @@ acceptanceTest("select", engine.memory, engine.sqlite, function(){
         attributes:["personId", "houseId", "name", "age"],
         rows:[
           [3, 102, "Fanny", 30]
+        ]
+      }, relationContents(smallerRelation))
+    })
+    
+    test("conjunction / and", function (){
+      var smallerRelation = 
+        this.$R(function(){
+          return select(relation("person"), and(eq(attr("person.houseId"), 101), eq(attr("person.age"), 5)))
+        }).perform()
+        
+      assert.equal({
+        name:"person",
+        attributes:["personId", "houseId", "name", "age"],
+        rows:[
+          [1, 101, "Jane", 5]
         ]
       }, relationContents(smallerRelation))
     })
